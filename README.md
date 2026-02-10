496e8955c898bb5305b868f4b18b10a6e60c63f7
Jayesh Akarte <jayeshakarte988@gmail.com>
2026-02-10 15:35:32 +0530

    Initial Flutter ecommerce project

---
+++ README.md
@@ -0,0 +1,3 @@
+# ecommarce_1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/ecommarce_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ecommarce_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
